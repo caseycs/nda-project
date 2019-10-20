3970484b729aa11ad2a7c7f68079a135e7e79676
Ilia Kondrashov <caseycs@gmail.com>
2019-10-20 23:37:42 +0200

    also include all previous steps when next one passed

--- src/Controllers/UserOnboardingStats.php
+++ src/Controllers/UserOnboardingStats.php
@@ -21,8 +21,17 @@ class UserOnboardingStats
         foreach ($this->dataProvider->getByWeek() as $item) {
             // @todo add data for all steps
             $result[] = [
-                $item->getWeek()->format('Y-W'),
-                $item->getStep1P(),
+                $item->getWeek()->format('Y \w\e\e\k W'),
+                [
+                    round($item->getStep1P() * 100, 2),
+                    round($item->getStep2P()* 100, 2),
+                    round($item->getStep3P()* 100, 2),
+                    round($item->getStep4P()* 100, 2),
+                    round($item->getStep5P()* 100, 2),
+                    round($item->getStep6P()* 100, 2),
+                    round($item->getStep7P()* 100, 2),
+                    round($item->getStep8P()* 100, 2),
+                ],
             ];
         }
 
--- src/Service/UserOnboardingStats/FileDataProvider.php
+++ src/Service/UserOnboardingStats/FileDataProvider.php
@@ -29,41 +29,6 @@ class FileDataProvider implements ProviderInterface
         return $this->aggregatePercentages($this->groupByWeek($this->reader->read()));
     }
 
-    private function groupByWeek(array $rawData): array
-    {
-        $result = [];
-        foreach ($rawData as $line => $user) {
-            $dateUnixtime = strtotime($user[1]);
-            $key = date('Y-W', $dateUnixtime);
-
-            if (!isset($result[$key])) {
-                // [date, count, [completePercentage, ...]]
-                $result[$key] = [$dateUnixtime, 0, []];
-            }
-
-            try {
-                $step = $this->stepCalculator->fromPercentage($user[2]);
-            } catch (\Exception $e) {
-                // @todo application logger should be used
-                error_log(
-                    sprintf(
-                        'fromPercentage failed on line %d (%s) with message "%s", skipping',
-                        $line,
-                        json_encode($user),
-                        $e->getMessage()
-                    )
-                );
-            }
-
-            if (!isset($result[$key][2][$step])) {
-                $result[$key][2][$step] = 0;
-            }
-            $result[$key][1]++;
-            $result[$key][2][$step]++;
-        }
-        return $result;
-    }
-
     /**
      * @param array $rawData
      * @return array|StepPercentagesPerWeek
@@ -87,6 +52,47 @@ class FileDataProvider implements ProviderInterface
 
             $result[] = new StepPercentagesPerWeek($date, ...$percentages);
         }
+        return $result;
+    }
+
+    private function groupByWeek(array $rawData): array
+    {
+        // small optimization - this array will be reused
+        $steps = array_fill_keys(range(1, 8), 0);
+
+        $result = [];
+        foreach ($rawData as $line => $user) {
+            $dateUnixtime = strtotime($user[1]);
+            $key = date('Y-W', $dateUnixtime);
+
+            if (!isset($result[$key])) {
+                // [date, total users count, [users completed step count, ...]]
+                $result[$key] = [$dateUnixtime, 0, $steps];
+            }
+
+            try {
+                $step = $this->stepCalculator->fromPercentage($user[2]);
+            } catch (\Exception $e) {
+                // @todo application logger should be used
+                error_log(
+                    sprintf(
+                        'fromPercentage failed on line %d (%s) with message "%s", skipping',
+                        $line,
+                        json_encode($user),
+                        $e->getMessage()
+                    )
+                );
+            }
+
+            // total users
+            $result[$key][1]++;
+
+            // filling in also all previous steps
+            for ($i = 1; $i <= $step; $i ++) {
+                $result[$key][2][$i]++;
+            }
+        }
+
         return $result;
     }
 }
